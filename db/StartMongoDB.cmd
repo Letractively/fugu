2784083ce4e927ab6e0592cd38dd8b59b8a22d1d
alexandr.lyalin <alexandr.lyalin@gmail.com>
2011-12-10 20:11:26 +0000

    - added fugu config - mongodb is now run as a console application

--- db/StartMongoDB.cmd
+++ db/StartMongoDB.cmd
@@ -1,9 +1,9 @@
-rem mongod.exe --dbpath data
+mongod.exe --dbpath data
 
-net stop MongoDB
-mongod --remove
-sc delete MongoDB
-mongod --logpath D:\dev\fugu\trunk\db\logs\logfilename.log --logappend --dbpath D:\dev\fugu\trunk\db\data --directoryperdb --install
-net start MongoDB
+rem net stop MongoDB
+rem mongod --remove
+rem sc delete MongoDB
+rem mongod --logpath D:\dev\fugu\trunk\db\logs\logfilename.log --logappend --dbpath D:\dev\fugu\trunk\db\data --directoryperdb --install
+rem net start MongoDB
 
 if not %ERRORLEVEL% == 0 pause
