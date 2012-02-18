ef406ab37af933476f6bcacff0b4e80a1815c3c3
alexandr.lyalin <alexandr.lyalin@gmail.com>
2012-02-18 18:19:09 +0000

    nginx: fugu module was added

---
+++ ngnix/fugu/ngx_http_fugu_module.c
@@ -0,0 +1,154 @@
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+#include <ngx_core.h>
+
+#include "hiredis/adapters/ngx_redis.h"
+
+static char *ngx_http_hello_world(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+
+static ngx_command_t  ngx_http_hello_world_commands[] = {
+
+  { ngx_string("hello_world"),
+    NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS,
+    ngx_http_hello_world,
+    0,
+    0,
+    NULL },
+
+    ngx_null_command
+};
+
+static u_char ngx_hello_string[] = "Hello, world!";
+
+static ngx_http_module_t  ngx_http_hello_world_module_ctx = {
+  NULL,                          /* preconfiguration */
+  NULL,                          /* postconfiguration */
+
+  NULL,                          /* create main configuration */
+  NULL,                          /* init main configuration */
+
+  NULL,                          /* create server configuration */
+  NULL,                          /* merge server configuration */
+
+  NULL,                          /* create location configuration */
+  NULL                           /* merge location configuration */
+};
+
+
+ngx_module_t ngx_http_hello_world_module = {
+  NGX_MODULE_V1,
+  &ngx_http_hello_world_module_ctx, /* module context */
+  ngx_http_hello_world_commands,   /* module directives */
+  NGX_HTTP_MODULE,               /* module type */
+  NULL,                          /* init master */
+  NULL,                          /* init module */
+  NULL,                          /* init process */
+  NULL,                          /* init thread */
+  NULL,                          /* exit thread */
+  NULL,                          /* exit process */
+  NULL,                          /* exit master */
+  NGX_MODULE_V1_PADDING
+};
+
+void getCallback(redisAsyncContext *c, void *r, void *privdata) 
+{
+    redisReply *reply = r;
+    if (reply == NULL) return;
+    printf("argv[%s]: %s\n", (char*)privdata, reply->str);
+
+    /* Disconnect after receiving the reply to GET */
+    redisAsyncDisconnect(c);
+}
+
+void connectCallback(const redisAsyncContext *c) {
+    ((void)c);
+    printf("connected...\n");
+}
+
+void disconnectCallback(const redisAsyncContext *c, int status) {
+    if (status != REDIS_OK) {
+        printf("Error: %s\n", c->errstr);
+    }
+    printf("disconnected...\n");
+}
+
+
+static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t *r)
+{
+    ngx_int_t    rc;
+    ngx_buf_t   *b;
+    //ngx_chain_t  out;
+
+    redisAsyncContext *redis_ctx = NULL;
+    redis_ctx = redisAsyncConnect("127.0.0.1", 6379);
+    redisNgxAttach((ngx_cycle_t*)ngx_cycle, redis_ctx);
+    redisAsyncSetConnectCallback(redis_ctx,connectCallback);
+    redisAsyncSetDisconnectCallback(redis_ctx,disconnectCallback);
+    redisAsyncCommand(redis_ctx, getCallback, (char*)"end-1", "GET key");
+    
+    // we response to 'GET' and 'HEAD' requests only 
+    if (!(r->method & (NGX_HTTP_GET|NGX_HTTP_HEAD))) {
+        return NGX_HTTP_NOT_ALLOWED;
+    }
+
+    //discard request body, since we don't need it here 
+    rc = ngx_http_discard_request_body(r);
+
+    if (rc != NGX_OK) {
+        return rc;
+    }
+    
+    /* set the 'Content-type' header */
+    r->headers_out.content_type_len = sizeof("text/html") - 1;
+    r->headers_out.content_type.len = sizeof("text/html") - 1;
+    r->headers_out.content_type.data = (u_char *) "text/html";
+
+    /* send the header only, if the request type is http 'HEAD' */
+    if (r->method == NGX_HTTP_HEAD) {
+        r->headers_out.status = NGX_HTTP_OK;
+        r->headers_out.content_length_n = sizeof(ngx_hello_string) - 1;
+
+        return ngx_http_send_header(r);
+    }
+
+    /* allocate a buffer for your response body */
+    b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
+    if (b == NULL) {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+
+    /* attach this buffer to the buffer chain */
+    //out.buf = b;
+    //out.next = NULL;
+
+    /* adjust the pointers of the buffer */
+    b->pos = ngx_hello_string;
+    b->last = ngx_hello_string + sizeof(ngx_hello_string) - 1;
+    b->memory = 1;    /* this buffer is in memory */
+    b->last_buf = 1;  /* this is the last buffer in the buffer chain */
+
+    /* set the status line */
+    r->headers_out.status = NGX_HTTP_OK;
+    r->headers_out.content_length_n = sizeof(ngx_hello_string) - 1;
+
+    /* send the headers of your response */
+    rc = ngx_http_send_header(r);
+
+    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
+        return rc;
+    }
+
+    /* send the buffer chain of your response */
+    return NGX_AGAIN;//ngx_http_output_filter(r, &out);
+}
+
+static char *ngx_http_hello_world(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{  
+    ngx_http_core_loc_conf_t  *clcf;
+    
+    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+    clcf->handler = ngx_http_hello_world_handler;
+
+    return NGX_CONF_OK;
+}
